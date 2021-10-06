18bb3f40c8bf0e535e30865130998cafcb5cd9fe
Bimal Poudel <anytizer@users.noreply.github.com>
2021-10-06 11:16:47 -0600

    Create Tone.cs

---
+++ swar/dtos/Tone.cs
@@ -0,0 +1,11 @@
+namespace dtos
+{
+    public class Tone
+    {
+        public string color { get; set; }
+        public string bgcolor { get; set; }
+        public string name { get; set; }
+        public string frequency { get; set; }
+        public string wavelength { get; set; }
+    }
+}
