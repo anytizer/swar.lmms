f0f6d66c75ebf172ff1228d72370544eb60d5136
Bimal Poudel <anytizer@users.noreply.github.com>
2021-10-06 11:18:23 -0600

    Create Formatter.cs

---
+++ swar/libraries/Formatter.cs
@@ -0,0 +1,49 @@
+using System.Collections.Generic;
+
+namespace libraries
+{
+    class Formatter
+    {
+        public string format_divisions(string line)
+        {
+            string outputs;
+            List<string> output = new List<string>();
+
+            string[] divisions = line.Split(new[] { '|', });
+            foreach (string d in divisions)
+            {
+                if (d != "")
+                {
+                    string notes = this.format_column(d);
+                    output.Add(notes);
+                }
+            }
+
+            outputs = string.Join(" | ", output.ToArray());
+            return outputs;
+        }
+
+        public string format_column(string column)
+        {
+            string outputs;
+            List<string> output = new List<string>();
+
+            string[] cells = column.Split(new[] { ' ', });
+            foreach (string cell in cells)
+            {
+                if (cell != "")
+                {
+                    output.Add(this.format_cell(cell));
+                }
+            }
+            outputs = string.Join(" ", output.ToArray());
+
+            return outputs;
+        }
+
+        public string format_cell(string cell)
+        {
+            return string.Format("{0,-5}", cell);
+        }
+    }
+}
