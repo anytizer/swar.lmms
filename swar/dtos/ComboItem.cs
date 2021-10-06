dca491da815825b96fe5a49e7e698388ace89816
Bimal Poudel <anytizer@users.noreply.github.com>
2021-10-06 11:13:49 -0600

    Create ComboItem.cs

---
+++ swar/dtos/ComboItem.cs
@@ -0,0 +1,13 @@
+namespace dtos
+{
+    public class ComboItem
+    {
+        public string Text { get; set; }
+        public string Value { get; set; }
+
+        public override string ToString()
+        {
+            return Text;
+        }
+    }
+}
