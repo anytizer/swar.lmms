54ddecb8a0369ce4c1672220aca549a195487e98
Bimal Poudel <anytizer@users.noreply.github.com>
2021-10-06 11:12:43 -0600

    Create Configurations.cs

---
+++ swar/configs/Configurations.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace configs
+{
+    public static class Configurations
+    {
+        public static string ReadWriteDirectory = "d:/desktop/xpt";
+
+        public static List<string> sargams()
+        {
+            List<string> sargams = new List<string>();
+
+            string[] lines = File.ReadAllLines(ReadWriteDirectory + "/sargams.txt", Encoding.UTF8);
+            foreach (string _line in lines)
+            {
+                string line = _line.Trim();
+                if (line != "" && !line.StartsWith("#") && File.Exists(line))
+                {
+                    sargams.Add(line);
+                }
+            }
+
+            return sargams;
+        }
+    }
+}
