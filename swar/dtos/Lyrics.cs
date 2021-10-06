e04fcd6fc75ea39345254da26d12ce4d9b85cd4f
Bimal Poudel <anytizer@users.noreply.github.com>
2021-10-06 11:14:17 -0600

    Create Lyrics.cs

---
+++ swar/dtos/Lyrics.cs
@@ -0,0 +1,10 @@
+namespace dtos
+{
+    public class Lyrics
+    {
+        public string filename { get; set; }
+        public string lyrics { get; set; }
+
+        public Signature signature { get; set; }
+    }
+}
